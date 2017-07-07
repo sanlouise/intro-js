d2a55d4cb583b0755904b052eebf835c18cda09a
sanlouise <sandra.adamshallie@gmail.com>
2017-07-07 11:14:29 -0400

    Initial and last commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let x = true;
+console.log(typeof x);
+
+let y = null;
+console.log(typeof y);
+
+let z = undefined;
+console.log(typeof z);
+
+let w = 8;
+console.log(typeof w);
+
+let v = 'Sandra';
+console.log(typeof v);
